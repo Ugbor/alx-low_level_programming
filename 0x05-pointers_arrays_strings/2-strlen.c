1d03e6b90fda9f29601bf91e4bb8ed818ce8d57a
ugbor moses <udensimoses2@gmail.com>
2022-09-20 02:50:15 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *_strlen - returns the length of a string
+ * _strlen - returns the length of a string
  * @s: string
  *Return: returns length as integer;
  */
@@ -15,3 +15,4 @@ int _strlen(char *s)
 
 	return (len);
 }
+
