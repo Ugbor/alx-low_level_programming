582e26b6ded682032cd4c9825bbc06ebf8c09531
ugbor moses <udensimoses2@gmail.com>
2022-09-22 00:56:22 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,7 @@
  * _strcmp - Function which compare two strings and
  * @s1: first string
  * @s2:second string
-* Return:
+ * Return:
  *                returns zero if s1 == s2
  *                returns negative number if s1 < s2
  *                returns positive number if s1 > s2
@@ -23,8 +23,6 @@ int _strcmp(char *s1, char *s2)
 			diff = s2[i];
 			break;
 		}
-
-
 		else if (s2[i] == '\0')
 		{
 			diff = s1[i];
